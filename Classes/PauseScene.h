f7e443aca18a940e5e3d73d9124a6c71b606c853
侯彤 <49677236+HouTong-s@users.noreply.github.com>
2022-05-05 21:51:43 +0800

    Delete Classes directory

--- Classes/PauseScene.h
+++
@@ -1,9 +0,0 @@
-﻿#include "cocos2d.h"
-
-class PauseScene:public cocos2d::Scene
-{
-public:
-    virtual bool init();  
-    static cocos2d::Scene* createScene(cocos2d::RenderTexture*);
-    CREATE_FUNC(PauseScene);
-};
--- Classes/ZombiesGenerator/AllGenerators.h
+++
@@ -1,6 +0,0 @@
-﻿#ifndef __AllGenerators_h__
-#define __AllGenerators_h__
-
-#include "FirstGenerator.h"
-
-#endif
