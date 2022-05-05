612bb15250e28330d25b4087d0b10ff224b14d73
侯彤 <49677236+HouTong-s@users.noreply.github.com>
2022-05-05 20:47:23 +0800

    Delete Classes directory

--- Classes/PauseScene.h
+++
@@ -1,9 +0,0 @@
-#include "cocos2d.h"
-
-class PauseScene:public cocos2d::Scene
-{
-public:
-    virtual bool init();  
-    static cocos2d::Scene* createScene(cocos2d::RenderTexture*);
-    CREATE_FUNC(PauseScene);
-};
